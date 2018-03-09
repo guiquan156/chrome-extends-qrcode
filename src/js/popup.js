a8a186ef8a8bf319105505dca00d4407f4c5ac94
unknown <lianguiquan@game.ntes>
2018-03-09 19:21:57 +0800

    init

---
+++ src/js/popup.js
@@ -0,0 +1,13 @@
+
+chrome.tabs.query({
+    active: true,
+    currentWindow: true
+}, function (tabs) {
+    var url = tabs[0].url;
+    var qrcode = new QRCode('qrcode', {
+        width: 160,
+        height: 160
+    });
+    qrcode.clear();
+    qrcode.makeCode(url);
+});
